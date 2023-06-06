ddd0633607c9008ba110cc126864e53602bf54f7
abdelfattah.bouzid7@gmail.com <abdelfattah.bouzid7@gmail.com>
2023-06-06 16:29:40 +0100

    module3_assignment_solution

--- app.js
+++ app.js
@@ -1,29 +1,65 @@
-(function (){
-    'use strict';
-   angular.module("myfirstApp",[])
-  .controller('myfirstController',function ($scope){
-       $scope.name1="";
-       $scope.name2="";
-       $scope.totalValue=0;
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItemsDirective)
+  .constant('ApiBasePath', 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json');
 
-       $scope.displayNumeric=function(){
-           var totalNameValue=calculateNumericForString($scope.name2);
-           $scope.totalValue=totalNameValue;
-       };
+NarrowItDownController.$inject = ['MenuSearchService'];
 
-     function calculateNumericForString(string){
-        var totalStringValue=0;
-        for( var i=0;i<string.length;i++){
-            totalStringValue+=string.charCodeAt(i);
-        }
-          return totalStringValue;
-      }
+function NarrowItDownController(MenuSearchService) {
+  var narrowCtrl = this;
+  narrowCtrl.searchTerm = '';
+  narrowCtrl.foundItems = [];
 
-  });
+  narrowCtrl.narrowDown = function () {
+    if (narrowCtrl.searchTerm.trim() !== '') 
+    {
+      var promise = MenuSearchService.getMatchedMenuItems(narrowCtrl.searchTerm);
 
+      promise.then(function (foundItems) {
+        narrowCtrl.foundItems = foundItems;
+      });
+    }
+    else 
+    {
+      narrowCtrl.foundItems = [];
+    }
+  };
 
+  narrowCtrl.removeItem = function (index) {
+    narrowCtrl.foundItems.splice(index, 1);
+  };
+}
 
-})();
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
 
+  service.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+      method: 'GET',
+      url: ApiBasePath
+    }).then(function (response) {
+      var menuItems = response.data;
 
+      var foundItems = menuItems.filter(function (item) {
+        return item.description.indexOf(searchTerm) !== -1;
+      });
 
+      return foundItems;
+    });
+  };
+}
+
+function FoundItemsDirective() {
+  var ddo = {
+    restrict: 'E',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    },
+    templateUrl: 'foundItems.html'
+  };
+
+  return ddo;
+}
